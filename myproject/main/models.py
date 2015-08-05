9d5c628f3e3f1cad462c761b9dc75b591cd63bc9
Alexander Silaev <assilaev@gmail.com>
2015-08-05 23:10:09 +0300

    Начальная модель бд

--- myproject/main/models.py
+++ myproject/main/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return str(self.name)
+
+
+# class CategoryWords(models.Model): # todo Добавить список ключевых слов для категорий
+#     pass
+
+
+class Achievement(models.Model):
+    category = models.ForeignKey(Category)
+    name = models.CharField(max_length=200)
+    short_text = models.TextField()
+    is_done = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    achieved_date = models.DateTimeField()
+
+    def __unicode__(self):
+        return str(self.name)
