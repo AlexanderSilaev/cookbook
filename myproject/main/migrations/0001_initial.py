365c9837ac1ccaaae428f88388cd4ed8a8984aba
Alexander Silaev <assilaev@gmail.com>
2015-08-05 20:27:03 +0000

    migrations main 0001

---
+++ myproject/main/migrations/0001_initial.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Achievement',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=200)),
+                ('short_text', models.TextField()),
+                ('is_done', models.BooleanField(default=True)),
+                ('created_date', models.DateTimeField(auto_now_add=True)),
+                ('achieved_date', models.DateTimeField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=200)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='achievement',
+            name='category',
+            field=models.ForeignKey(to='main.Category'),
+        ),
+    ]
