63fcd630d94dab713e4336dcd5837de0b8381e89
Alexander Silaev <assilaev@gmail.com>
2015-08-10 20:37:58 +0300

    Статика для админки

--- myproject/main/models.py
+++ myproject/main/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=200)
 
-    def __unicode__(self):
+    def __str__(self):
         return str(self.name)
 
 
@@ -22,5 +22,5 @@ class Achievement(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     achieved_date = models.DateTimeField()
 
-    def __unicode__(self):
+    def __str__(self):
         return str(self.name)
